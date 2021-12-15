30b1bfff23694b6e72cc9384a45655d0203de8ea
danielherreracorrea <93483425+danielherreracorrea@users.noreply.github.com>
2021-12-14 23:49:01 -0500

    intentos

--- Punto 2.py
+++ Punto 2.py
@@ -19,17 +19,36 @@ def Pupila(w_l, dx0, N):
     In this case: plane wave and circular aperture 
     """
     na=0.25
-    dx0=w_l*20*(1e3)/(4*N*dx0)
+#    dx0=w_l*20*(1e3)/(4*N*dx0)
     dy0=dx0
     N=2*N
     x=np.arange(-N,N)
     y=np.arange(-N,N)
     x,y=np.meshgrid(x,y)
-#    x=x*dx0
-#    y=y*dy0
-    lim=na*20*1e3/np.sqrt(1-0.25*0.25)      #20000 um= 20 mm
-#    lim=1e5
-    U_matrix=(x)**2 + (y)**2
+    x=x*dx0
+    y=y*dy0
+    #lim=na*20*1e3/np.sqrt(1-0.25*0.25)      #20000 um= 20 mm
+    lim=2*1e5
+    U_matrix=(x/(w_l*2*1e5))**2 + (y/(w_l*2*1e5))**2
+    U_matrix[np.where(U_matrix<=lim)]=1
+    U_matrix[np.where(U_matrix>lim)]=0
+    print (lim, 512*w_l*200*1e3)
+    return U_matrix
+
+def Imagen(w_l, dx0, N):
+    """
+    Incident wave and transmittance function
+    In this case: plane wave and circular aperture 
+    """
+    #dx0=dx0.     #Pixeles 10 veces más pequeños. dx0=2.5 um
+    N=2*N
+    dy0=dx0
+    x=np.arange(-N,N)
+    y=np.arange(-N,N)
+    x,y=np.meshgrid(x,y)
+
+    lim=1e3         #Tamaño 10 mm
+    U_matrix=(x*dx0*0.1)**2 + (y*dy0*0.1)**2
     U_matrix[np.where(U_matrix<=lim)]=1
     U_matrix[np.where(U_matrix>lim)]=0
 #    print (lim, 512*w_l*200*1e3)
@@ -37,29 +56,30 @@ def Pupila(w_l, dx0, N):
 
 
 w_l=0.533          #(533nm)
-dx0=2.5              #2.5um tamaño de pixel
+dx0=25.              #2.5um tamaño de pixel
 N=M=int(512/2)     #Number of pixels
 
 
 
 
 
-U_0=cv2.imread('cameraman.png',0)
-    
+#U_0=cv2.imread('cameraman.png',0)
+U_0=Imagen(w_l, dx0, N) 
+   
 r=int(512/2)
-U_0 = cv2.copyMakeBorder(U_0,r,r,r,r,cv2.BORDER_CONSTANT)
-U_0=U_0/10
+#U_0 = cv2.copyMakeBorder(U_0,r,r,r,r,cv2.BORDER_CONSTANT)
 
-#U_1=np.fft.fft2(0.1*U_0*dx0**2)
+
+#U_1=np.fft.fft2(U_0)
 P=Pupila(w_l, dx0, N)
-#P_FT=FT(P/(w_l*200000), w_l, dx0)
+P_FT=np.fft.fft2(P/(w_l*2*1e5))
 
-U_1=np.fft.fftshift(np.fft.fftn(U_0*dx0**2))
+U_1=(np.fft.fftn(U_0*(dx0)**2))
 
 
 
-Uf=U_1*P/(w_l*20*1e3)
-Uf=np.fft.fftn(Uf)
+Uf=U_1*P*0.1
+Uf=(1/1j*w_l*2*1e5)*np.fft.fftshift(np.fft.fftn(Uf*dx0**2))
 I1=np.log(np.abs(U_1)**2)
 I=(np.abs(Uf)**2) 
 angle=np.angle(Uf) 
