5f7a1265c2c0d2385807cccb03a8e505d2c50580
alegomezram <alegomezram@unal.edu.co>
2021-12-15 00:58:17 -0500

    Fresnel algorithm added
    To propagate the hologram to the object plane we use Fresnel diffraction

--- Punto 3.py
+++ Punto 3.py
@@ -38,6 +38,26 @@ def Ref(w_length, deltax0, deltay0, M,N):
     R=np.fft.fftshift(R)
 
     return R
+
+def Fresnel(Field, z, deltax0, deltay0, w_length, deltax, deltay, M, N):
+    """
+    Fresnel function
+    """
+    
+    k= 2*np.pi/w_length
+    
+    x =np.arange(-M,M)
+    y =np.arange(-N,N)
+    
+    X,Y = np.meshgrid(x,y)
+    
+    expFT = np.exp(((1j*k)/(2*z))*(((X*deltax0)**2)+((Y*deltay0)**2)))
+    F= np.fft.fft2(Field*expFT*deltax0*deltay0)
+    
+    amp = np.exp(((1j*k)/(2*z))*((X*deltax)**2+(Y*deltay)**2))*(np.exp(1j*k*z)/(1j*w_length*z))
+    F= np.fft.fftshift(F)*amp
+    
+    return F
 """
 U=incident wave
 z=entrance plane to detector plane distance
@@ -53,7 +73,7 @@ M*2xN*2=number of pixels entrance plane
 
 M=1024 #Number of pixels=M*2
 N=1024 #Number of pixels=N*2
-z=2500  #(2.5mm)
+z=73000  #(73mm)
 w_length=0.633   #All units in um
 deltax0=3.18 
 deltay0=3.18
@@ -63,6 +83,12 @@ deltay=(w_length*z)/(2*M*deltay0)
 
 print("deltax:",deltax)
 print("deltay:",deltay)
+
+lim=2*N*(deltax0**2)/w_length  #Limit of z in FT
+print ("lim:",lim)
+if z<lim:
+    print("z limit exceeded")
+
 tic=time.time()
 
 T=Tmatrix(deltax0, deltay0, M,N)
@@ -72,14 +98,22 @@ FTholo= np.fft.fftshift(FTholo)
 Filter=T*FTholo
 InvFT=np.fft.ifft2(Filter)
 R=Ref(w_length, deltax0, deltay0, M,N)
+U1=InvFT*R
+Reconst=Fresnel(U1, z, deltax0, deltay0, w_length, deltax, deltay, M, N)
 
+#Intensity
 Iholo=np.log(np.abs(FTholo)**2)
 IFilter=np.log((np.abs(Filter)**2)+0.0001)
 IInvFT=np.abs(InvFT)**2
 IR=np.log(np.abs(R)**2)
+IU1=np.log(np.abs(U1)**2)
+IReconst=(np.abs(Reconst)**2)
 
+#Figures
 plt.imsave("Transmittance.png",T, cmap='gray') 
 plt.imsave("Fourier Transform Hologram.png",Iholo, cmap='gray')   
 plt.imsave("FT Hologram filtered.png",IFilter, cmap='gray')  
 plt.imsave("Inverse FT filtered.png",IInvFT, cmap='gray')  
-plt.imsave("FT Reference.png",IR, cmap='gray')   
+plt.imsave("FT Reference.png",IR, cmap='gray')  
+plt.imsave("U+1.png",IU1, cmap='gray')  
+plt.imsave("Reconstruction.png",IReconst, cmap='gray') 
